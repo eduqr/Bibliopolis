994f6ac42ec4f3d0a129aa76a32a6523c73389d3
eduqr <simplyy017@gmail.com>
2023-07-15 19:30:03 -0500

    Primer commit

---
+++ bibliopolis/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bibliopolis.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL("server = localhost; database = bibliopolisDB; user = root; password =");
+        }
+    }
+}
